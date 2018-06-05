87b53c936891321257170d9ed37c45a8e6726745
MPC2grim911 <murphc1@uci.edu>
2018-06-04 20:29:44 -0700

    Added scripts of scene change buttons
    the only scene not set to change into is "scoreboard" which is the action that should occur at "Game Over"

---
+++ BottleRocket/Assets/Scripts/HomeButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HomeButton : MonoBehaviour {
+	public GameObject button;
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		TaskOnClick ();
+	}
+
+	void TaskOnClick(){
+		if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == button)
+		{
+			SceneManager.LoadScene ("Home");
+		}
+	}
+}
