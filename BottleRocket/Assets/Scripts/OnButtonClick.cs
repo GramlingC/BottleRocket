17a0050b694f0570a48f1652ca7dd6f86776fcf0
Tina <xuanfeng1996@gmail.com>
2018-06-12 16:53:06 -0700

    Basic Mobile BS

---
+++ BottleRocket/Assets/Scripts/OnButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnButtonClick : MonoBehaviour {
+    
+    public void OnButtonDown(string command)
+    {
+        if (command == "start")
+            SceneManager.LoadScene("Game");
+
+        else if (command == "quit")
+            Application.Quit();
+
+        
+
+
+
+    }
+}
