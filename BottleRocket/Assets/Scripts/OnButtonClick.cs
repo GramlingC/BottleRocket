adec81a442d2e14990fbe7704631aec37ef1e0df
MPC2grim911 <murphc1@uci.edu>
2018-06-13 19:03:45 -0700

    updated OnButtonClick

--- BottleRocket/Assets/Scripts/OnButtonClick.cs
+++ BottleRocket/Assets/Scripts/OnButtonClick.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OnButtonClick : MonoBehaviour {
-    public int numPlayer = 1;
+    public GameObject numPlayer; //temporary placement
 
     public void OnButtonDown(string command)
     {
@@ -14,18 +14,22 @@ public class OnButtonClick : MonoBehaviour {
         else if (command == "quit")
             Application.Quit();
 
-        else if (command == "home")
+        else if (command == "home") //return to main menu
             SceneManager.LoadScene("MainMenu");
 
         else if (command == "about")
             SceneManager.LoadScene("Rules");
 
-        else if (command == "add") { if (numPlayer != 10) numPlayer++; }
+        else if (command == "partySize") //go to change number of players
+            SceneManager.LoadScene("PartySize");
 
-        else if (command == "sub") { if (numPlayer != 0) numPlayer--; }
+        else if (command == "add") { /*if (numPlayer != 10) numPlayer++;*/ }
+
+        else if (command == "sub") { /*if (numPlayer != 0) numPlayer--;*/ }
 
         else if (command == "prompt") //to ask players to play again
             SceneManager.LoadScene("EndGame");
+
 
     }
 }
