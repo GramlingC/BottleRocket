0d3276d182b6f5fd1b153887909856f46caa24c1
Christian Gramling <gramlinc@uci.edu>
2018-06-16 08:32:15 -0700

    Simple audio manager

---
+++ BottleRocket/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playSound(int i)
+	{
+		transform.GetComponentsInChildren<AudioSource>()[i].Play();
+	}
+
+	public void stopSound(int i)
+	{
+		transform.GetComponentsInChildren<AudioSource>()[i].Stop();
+	}
+	
+}
